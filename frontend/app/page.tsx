7e2ab26f74c05ceadd998278c3e80e7be10ecf11
mmschulz <145290876+mmschulz@users.noreply.github.com>
2025-09-02 22:59:47 +0000

    changed name

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -473,7 +473,7 @@ export default function Home() {
                     lineHeight: 1.2,
                   }}
                 >
-                  Hi, I&apos;m John Doe
+                  Hi, I&apos;m Madison Schulz
                   {/* Job title with gradient effect */}
                   <Typography
                     variant="h2"
